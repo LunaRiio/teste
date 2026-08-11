3403531a3fc75105cbec15b643da7aa62ebfe263
Luna Rio <lunahvir@gmail.com>
2026-08-11 13:12:43 -0300

    teste

---
+++ app.js
@@ -0,0 +1,4 @@
+let listaGenerica = [];
+let linguagensDeProgramcao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramcao.push('Java', 'Ruby', 'GoLang');
+console.log( linguagensDeProgramcao[2]);
